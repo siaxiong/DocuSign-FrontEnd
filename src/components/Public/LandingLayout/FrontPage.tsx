fc04140acb6ab336147b93390c744e8a5002869d
siaxiong <siaxiong52@gmail.com>
2022-12-14 20:01:15 -0800

    Removed video tutorial

--- src/components/Public/LandingLayout/FrontPage.tsx
+++ src/components/Public/LandingLayout/FrontPage.tsx
@@ -7,11 +7,10 @@ function FrontPage() {
         <ul className={FPStyle["front-page"]}>
           <li className={FPStyle["front-page__child"]}>
             <div className={FPStyle["front-page__summary"]}>
-              <h2>Are you a manager at your workplace? Do you manage a lot of paper works?</h2>
-              <p>DocuSign eSignature allows anyone with an account to send PDF forms to another person who has an account. 
-                We know it's painful to use regular emailing services to send a form to someone, they have to download the form,
-                sign it, reupload it to their emailing service, and send it back to you. That's just too much work in the 2022!
-                Here at DocuSign you can expect an easy workflow to sign PDF forms.
+              <h2>What is DocuSign eSignature?</h2>
+              <p>DocuSign eSignature is dedicated service for sending, tracking, and signing PDFs. This is
+                especially usefully in teams and in organizations that need a centralize way to sign and track
+                a lot of paperworks.
               </p>
             </div>
             <LoginForm/>
@@ -35,12 +34,6 @@ function FrontPage() {
                 <p>3. The receiver click send to finish signing the PDF
                 </p>
               </div>
-          </li>
-          <li className={FPStyle["front-page__child"]}>
-            <div className={FPStyle["front-page__summary"]}>
-              <h2>Watch this video</h2>
-              <div className={FPStyle["loom__container"]}><iframe src="https://www.loom.com/embed/a1b73005d58d4e93aff3b2ce8cb94da0"></iframe></div>
-            </div>
           </li>
           <li className={FPStyle["front-page__child"]}>
             <div className={FPStyle["front-page__summary"]}>
